e9a8e794c9fab65f76d1fb8a9eb11c628d3b83f8
mamismeda <amamismeda@gmail.com>
2022-01-29 17:07:18 +0400

    end of first challenge in a new section

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,4 +1,14 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
+let arr = [17, 21, 23];
+let arr2 = [12, 5, -5, 0, 4];
 
+let printForecast = function (ar) {
+  let str = '';
+  for (let i = 0; i < ar.length; i++){
+      str = str + `${arr[i]} C in ${i + 1} days ... `;
+  }
+  console.log("..." + str);
+};
 
+printForecast(arr);
