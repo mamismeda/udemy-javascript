411a604dd13dcc6101a9ebaf8438d1dd2a4c961e
mamismeda <amamismeda@gmail.com>
2022-01-29 17:07:33 +0400

    end of first challenge in a new section

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,6 +1,6 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
-let arr = [17, 21, 23];
+/* let arr = [17, 21, 23];
 let arr2 = [12, 5, -5, 0, 4];
 
 let printForecast = function (ar) {
@@ -11,4 +11,6 @@ let printForecast = function (ar) {
   console.log("..." + str);
 };
 
-printForecast(arr);
+printForecast(arr); */
+
+// END of first challenge in a new section 
