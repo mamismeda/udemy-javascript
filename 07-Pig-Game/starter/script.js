5c152bba042cfe1f1004779858efe7d821bf7636
mamismeda <amamismeda@gmail.com>
2022-02-02 20:00:39 +0400

    changed game rules

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -63,7 +63,7 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer];
 
     //2. check if players score its >= 100
-    if (scores[activePlayer] >= 50) {
+    if (scores[activePlayer] >= 100) {
       //finish the game
       playing = false;
       diceEl.classList.add('hidden');
