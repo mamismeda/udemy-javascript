d1c2bf861214f4111238f04517aee82bb5d56d3a
mamismeda <amamismeda@gmail.com>
2022-02-06 17:30:06 +0400

    beginning to anderstund this.keyword

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
@@ -28,4 +28,55 @@ function calcAge(birthYear) {
 }
 
 const firstName = 'jonas';
-calcAge(1991);
+calcAge(1991);*/
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+/*
+
+//variables
+
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'jonas';
+let job = 'teacher';
+const year = 1991;
+
+//functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+
+console.log(undefined);
+if(!numProducts) deleteShoppingCard();
+
+var numProducts = 10;
+
+function deleteShoppingCard(){
+  console.log("all products deleted!");
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x=== window.x);
+console.log(y=== window.y);
+console.log(z=== window.z);
+*/
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
