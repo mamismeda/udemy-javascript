9600e4e83e7afa051fce8c530925c11ee7e482b9
mamismeda <amamismeda@gmail.com>
2022-02-06 13:35:08 +0400

    scopint in every situation

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `${firstName} you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const firstName = 'Steven'
+    //   output = 'NEW OUTPUT';
+      const str = `oh , and you are a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    
+    }
+    // console.log(str)
+    console.log(millenial);
+    // add(2, 3);
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'jonas';
+calcAge(1991);
