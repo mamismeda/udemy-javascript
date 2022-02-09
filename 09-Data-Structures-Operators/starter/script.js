87f33bc0ea3213df30d15680459cfe6f2de9508f
mamismeda <amamismeda@gmail.com>
2022-02-09 20:59:50 +0400

    spread operators

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -30,13 +30,65 @@ const restaurant = {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDelivery : function({starterIndex,mainIndex,time,address}){
+  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
     console.log(`order recieved ! ${this.starterMenu[starterIndex]}
     and ${this.mainMenu[mainIndex]} will be delivered to ${address} at
     ${time}`);
   },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`here is your delicius pasta with ${ing1}, ${ing2}, ${ing3}`);
+  },
 };
 
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+
+console.log(...newArr);
+
+const newMenu = [...restaurant.mainMenu, 'Goncci'];
+console.log(newMenu);
+
+// Copy array
+
+const mainMenuCopy = [...restaurant.mainMenu];
+
+// Join 2 arays or more
+
+const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(menu);
+
+// Iterables : arrays, strings, maps, sets. NOT objects
+
+const str = 'jonas';
+const letters = [...str, '', 's'];
+console.log(letters);
+//real world example
+// const ingredients = [prompt("let's make pasta! ingredient 1"),prompt("let's make pasta! ingredient 1"),
+// prompt("let's make pasta! ingredient 1")];
+// console.log(ingredients);
+
+// restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
+// restaurant.orderPasta(...ingredients);
+
+
+// Objects
+
+const newRestaurant = {foundIn : 1998, ...restaurant, founder : 'Guiseppe'};
+console.log(newRestaurant);
+
+const restaurantCopy = {...restaurant};
+restaurantCopy.name = 'Ristorante Roma';
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
+
+
+/*
+//////////////////////////////////////////////////////////
 restaurant.orderDelivery({
   time: '22:30',
   address : 'VIa del sole, 21',
@@ -68,6 +120,9 @@ console.log(a, b);
 // Nested objects
 const {fri: {open: o, close: c} } = openingHours;
 console.log(o, c);
+
+
+*/
 
 // const arr = [2, 3, 4];
 // const a = arr[0];
