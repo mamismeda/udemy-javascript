15bb198db122713c5c974b9808b00738f14c431c
mamismeda <amamismeda@gmail.com>
2022-02-26 18:45:25 +0400

    fill and from array methods

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -538,6 +538,8 @@ const overalBalance2 = accounts
 console.log(overalBalance2);
 */
 
+/*
+
 // sort method mutates original array // string
 const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
 console.log(owners.sort());
@@ -565,3 +567,38 @@ console.log(movements);
 // });
 movements.sort((a, b) => b - a);
 console.log(movements);
+
+*/
+const arr = [1, 2, 3, 4, 5, 6, 7];
+
+// Empty arrays + fill method ~!!!
+const x = new Array(7);
+console.log(x);
+
+x.fill(1);
+
+// like slice method
+x.fill(1, 3, 5);
+console.log(x);
+
+arr.fill(23, 2, 6);
+console.log(arr);
+
+// Array.from
+
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+
+
+
+labelBalance.addEventListener('click', function(){
+  const movementsUI = Array.from(document.
+    querySelectorAll('.movements__value'),
+    el => Number(el.textContent));
+  
+  console.log(movementsUI);
+
+})
