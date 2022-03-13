169c8dffd88f72e04e2057a6fa7cb527f68cf20b
mamismeda <amamismeda@gmail.com>
2022-03-13 20:50:16 +0400

    revealing section on scrooling

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -153,7 +153,6 @@ window.addEventListener('scroll', function(e){
 
 const header = document.querySelector('.header');
 const navHeight = nav.getBoundingClientRect().height;
-console.log(navHeight);
 
 const stickyNav = function(entries) {
   const [entry] = entries;
@@ -169,6 +168,27 @@ const headerObserver = new IntersectionObserver(stickyNav, {
   rootMargin : `${navHeight}px`,
 });
 headerObserver.observe(header);
+
+// Reveal sections smooth
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function(entries, observer){
+  const [entry] = entries;
+  console.log(entry);
+  if(!entry.isIntersecting) return ;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+}
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root : null,
+  treshold : 0.15,
+})
+allSections.forEach(function(section){
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+})
 
 ////////////////////////////////////////////////////
 
